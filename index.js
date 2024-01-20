293c8b29f8d7f7b98fe1f88349ed746f97c2fe43
shuuki <733886-shuuki@users.noreply.replit.com>
2024-01-20 23:33:22 +0000

    database now tracks messages

--- index.js
+++ index.js
@@ -1,64 +1,64 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-const sqlite3 = require('sqlite3').verbose();
+			const express = require('express');
+			const http = require('http');
+			const socketIo = require('socket.io');
+			const sqlite3 = require('sqlite3').verbose();
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
+			const app = express();
+			const server = http.createServer(app);
+			const io = socketIo(server);
 
-let db = new sqlite3.Database('./database.db', (err) => {
-	if (err) {
-		return console.error(err.message);
-	}
-	console.log('Connected to the SQlite database.');
-});
-
-db.get(`SELECT name FROM sqlite_master WHERE type='table' AND name='numbers';`, (err, row) => {
-	if (err) {
-		return console.error(err.message);
-	}
-	if (!row) {
-		db.run('CREATE TABLE numbers(value INTEGER)', function(err) {
-			if (err) {
-				return console.log(err.message);
-			}
-			db.run(`INSERT INTO numbers(value) VALUES(?)`, [0], function(err) {
+			let db = new sqlite3.Database('./database.db', (err) => {
 				if (err) {
-					return console.log(err.message);
+					return console.error(err.message);
 				}
+				console.log('Connected to the SQlite database.');
 			});
-		});
-	}
-});
 
-app.get('/', (req, res) => {
-		res.sendFile(__dirname + '/index.html');
-});
+			db.serialize(() => {
+				db.run(`CREATE TABLE IF NOT EXISTS numbers(value INTEGER)`);
+				db.run(`CREATE TABLE IF NOT EXISTS messages(id INTEGER PRIMARY KEY, text TEXT)`);
+			});
 
-io.on('connection', (socket) => {
-		db.get('SELECT value FROM numbers', [], (err, row) => {
-			if (err) {
-				return console.error(err.message);
-			}
-			socket.emit('number', row.value);
-		});
+			app.get('/', (req, res) => {
+					res.sendFile(__dirname + '/index.html');
+			});
 
-		socket.on('increment', () => {
-				db.run(`UPDATE numbers SET value = value + 1`, function(err) {
-					if (err) {
-						return console.error(err.message);
-					}
+			io.on('connection', (socket) => {
 					db.get('SELECT value FROM numbers', [], (err, row) => {
 						if (err) {
 							return console.error(err.message);
 						}
-						io.sockets.emit('number', row.value);
+						socket.emit('number', row.value);
 					});
-				});
-		});
-});
 
-server.listen(3000, () => {
-		console.log('Server is running on port 3000');
-});
+					socket.on('increment', () => {
+							db.run(`UPDATE numbers SET value = value + 1`, function(err) {
+								if (err) {
+									return console.error(err.message);
+								}
+								db.get('SELECT value FROM numbers', [], (err, row) => {
+									if (err) {
+										return console.error(err.message);
+									}
+									io.sockets.emit('number', row.value);
+								});
+							});
+					});
+
+					socket.on('submit', (message) => {
+							db.get('SELECT value FROM numbers', [], (err, row) => {
+								if (err) {
+									return console.error(err.message);
+								}
+								db.run(`INSERT OR IGNORE INTO messages(id, text) VALUES(?, ?)`, [row.value, message], function(err) {
+									if (err) {
+										return console.error(err.message);
+									}
+								});
+							});
+					});
+			});
+
+			server.listen(3000, () => {
+					console.log('Server is running on port 3000');
+			});
